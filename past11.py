26a68e0b1890978fab1d99b78b4d9de4da89aad6
Isacpaulo <isacpaulo.com@gmail.com>
2023-01-31 21:39:01 -0300

    Segundo commit

---
+++ past11.py
@@ -0,0 +1,11 @@
+m = str
+f = str
+sexo = str
+
+sexo = str(input("digite f para feminino e m para masculino"))
+if sexo == "f":
+    print("feminino")
+elif sexo == "m":
+    print("masculino")
+else:
+    print("sexo invalido")
