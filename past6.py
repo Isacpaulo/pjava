bc59b570fdef36c20a74564b0cae4c23033b03c7
Isacpaulo <isacpaulo.com@gmail.com>
2023-01-31 21:41:04 -0300

    Terceiro commit

---
+++ past6.py
@@ -0,0 +1,9 @@
+dobro = 0
+altura = 0
+lado = 0
+area = 0
+lado = int(input("informe o lado do quadrado"))
+area = lado * lado
+dobro = (lado * lado)*2
+print("a area é",area)
+print("o dobro é",dobro)
